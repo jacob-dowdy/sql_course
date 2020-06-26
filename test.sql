86d3b8933d989d36826d75d7facf1938383dee4f
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-06-26 01:08:46 -0400

    added 5 rows to test

--- test.sql
+++ test.sql
@@ -59,4 +59,21 @@ SELECT * FROM practice;
 
 DROP TABLE practice;
 
+-- INSERT DATA INTO A TABLE
+
+/* 
+
+INSERT INTO tablename (columnname1, columnname2, columnname3)
+VALUES ('value1', 'value2', 'value3');
+
+*/
+
+INSERT INTO examples (first_name, last_name, email, nationality, age)
+VALUES('David','Mitchell','dmitch@gmail.com','GBR',43);
+
+SELECT * FROM examples;
+
+INSERT INTO examples (first_name, last_name, email, nationality, age)
+VALUES('Emily','Watson','ewatson@gmail.com','USA',29),('Theo','Scott','tscott@gmail.com','AUS',33),('Emily','Smith','esmith@gmail.com','GBR',29),('Jim','Burr','jburr@gmail.com','USA',54);
+
 
