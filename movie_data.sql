f62a0688c1bdeaf40f3c1ea47df8a467179208bb
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-04 02:57:13 -0400

    Added FETCH section

--- movie_data.sql
+++ movie_data.sql
@@ -704,3 +704,19 @@ SELECT * FROM movie_revenues
 ORDER BY revenue_id
 LIMIT 8 OFFSET 2;
 
+-- Using Fetch
+
+/*
+SELECT columname1 FROM table1
+FETCH FIRST 1 ROW ONLY;
+*/
+
+SELECT movie_id, movie_name FROM movies
+FETCH FIRST ROW ONLY;
+
+SELECT movie_id, movie_name FROM movies
+FETCH FIRST 10 ROW ONLY;
+
+SELECT movie_id, movie_name FROM movies
+OFFSET 8 ROWS
+FETCH FIRST 10 ROW ONLY;
