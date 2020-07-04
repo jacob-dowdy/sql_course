69a81d704e927ad72ffd3e95b799366c4d819095
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-04 14:26:47 -0400

    added concatenate section

--- movie_data.sql
+++ movie_data.sql
@@ -780,3 +780,24 @@ WHERE last_name LIKE 'A%'
 ORDER BY surname;
 
 SELECT * FROM directors;
+
+-- Using concatenate
+
+/*
+SELECT 'string1' || 'string2' AS new_string;
+
+SELECT CONCAT(column1, column2) AS new_column FROM tablename;
+
+SELECT CONCAT_WS(' ', column1, column2) AS new_column FROM tablename;
+*/
+
+SELECT 'concat' || 'together' AS new_string;
+SELECT 'concat' || ' ' || 'together' AS new_string;
+
+SELECT CONCAT(first_name, last_name) AS full_name FROM actors;
+
+SELECT CONCAT(first_name,' ', last_name) AS full_name FROM actors;
+
+SELECT CONCAT_WS(' ', first_name, last_name, date_of_birth) AS full_name FROM actors;
+
+
