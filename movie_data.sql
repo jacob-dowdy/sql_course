2b252443de5cbe106a049314862c033179996c68
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-07 00:04:48 -0400

    added MIN and MAX section

--- movie_data.sql
+++ movie_data.sql
@@ -840,3 +840,27 @@ WHERE domestic_takings > 100.0;
 
 SELECT SUM(movie_length) FROM movies
 WHERE movie_lang = 'Chinese';
+
+-- Aggregate Functions : MIN and MAX
+
+/*
+SELECT MAX(columname) FROM tablename;
+SELECT MIN(columname) FROM tablename;
+*/
+
+SELECT MAX(movie_length) FROM movies;
+SELECT MIN(movie_length) FROM movies;
+
+SELECT MIN(movie_length) FROM movies
+WHERE movie_lang = 'Japanese';
+
+SELECT MAX(release_date) FROM movies;
+SELECT MIN(release_date) FROM movies;
+
+SELECT MAX(movie_name) FROM movies;
+SELECT MIN(movie_name) FROM movies;
+
+
+
+
+
