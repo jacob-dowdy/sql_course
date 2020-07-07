ea2200c08ebcd65d7d98901f2ccab139ef4279a9
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-06 23:45:49 -0400

    added SUM section

--- movie_data.sql
+++ movie_data.sql
@@ -827,3 +827,16 @@ SELECT COUNT(international_takings) FROM movie_revenues;
 SELECT COUNT(*) FROM movies
 WHERE movie_lang = 'English';
 
+-- Aggregate Functions : SUM
+
+/*
+SELECT SUM(columname) FROM tablename;
+*/
+
+SELECT SUM(domestic_takings) FROM movie_revenues;
+
+SELECT SUM(domestic_takings) FROM movie_revenues
+WHERE domestic_takings > 100.0;
+
+SELECT SUM(movie_length) FROM movies
+WHERE movie_lang = 'Chinese';
