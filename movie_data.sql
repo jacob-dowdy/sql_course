2b432080350b3938ff40c02dbeecbbda703cc927
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-07 00:22:59 -0400

    added challenge 5

--- movie_data.sql
+++ movie_data.sql
@@ -869,5 +869,23 @@ SELECT AVG(columname) FROM tablename;
 SELECT AVG(movie_length) FROM movies
 WHERE age_certificate = '18';
 
+-- Challenge 5
+
+SELECT COUNT(*) FROM actors
+WHERE date_of_birth > '1970-01-01';
+
+SELECT * FROM movies;
+
+SELECT MAX(domestic_takings) FROM movie_revenues;
+SELECT MIN(domestic_takings) FROM movie_revenues;
+
+SELECT SUM(movie_length) FROM movies
+WHERE age_certificate = '15';
+
+SELECT COUNT(*) FROM directors
+WHERE nationality = 'Japanese';
+
+SELECT AVG(movie_length) FROM movies
+WHERE movie_lang = 'Chinese';
 
 
