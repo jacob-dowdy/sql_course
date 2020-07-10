5c98022fd28547e554ec4ffa488846dc5fdd93d8
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-10 01:21:46 -0400

    added operators section

--- movie_data.sql
+++ movie_data.sql
@@ -946,3 +946,21 @@ SELECT movie_lang, SUM(movie_length) FROM movies
 GROUP BY movie_lang
 HAVING SUM(movie_length) > 500;
 
+-- Using Mathematical Operators
+
+/*
+
++ = / * %
+
+*/
+
+SELECT 5 + 6 AS addition;
+SELECT 8 - 3 AS subtraction;
+SELECT 35 / 3 AS division;
+SELECT 4 * 6 AS multiplication;
+
+SELECT 8 % 2 AS modulus;
+
+SELECT * FROM movie_revenues;
+
+SELECT movie_id, (domestic_takings + international_takings) AS total_takings FROM movie_revenues;
