01543872460a228671dc1d0514c50196c06611f3
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-10 00:04:32 -0400

    added HAVING section

--- movie_data.sql
+++ movie_data.sql
@@ -914,4 +914,24 @@ SELECT movie_lang, MIN(movie_length), MAX(movie_length) FROM movies
 WHERE age_certificate = '15'
 GROUP BY movie_lang;
 
+-- HAVING Clauses
+
+/*
+SELECT column1, AGGFUN(column2) FROM tablename
+GROUP BY column1
+HAVING AGGFUN(column3) = value;
+*/
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+WHERE movie_length > 120
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
+
+
+
+
 
