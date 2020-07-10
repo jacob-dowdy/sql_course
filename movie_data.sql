1e58ef9cd1bef4d4379cf98f7f85b2f26b0e1c71
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-09 23:54:21 -0400

    added GROUPING section

--- movie_data.sql
+++ movie_data.sql
@@ -888,4 +888,30 @@ WHERE nationality = 'Japanese';
 SELECT AVG(movie_length) FROM movies
 WHERE movie_lang = 'Chinese';
 
+-- Grouping data
+
+/*
+SELECT column1, AGGFUN(column2) FROM tablename
+GROUP BY column1;
+*/
+
+SELECT COUNT(movie_lang) FROM movies;
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang, AVG(movie_length) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang, age_certificate, AVG(movie_length) FROM movies
+GROUP BY movie_lang, age_certificate;
+
+SELECT movie_lang, age_certificate, AVG(movie_length) FROM movies
+WHERE movie_length > 120
+GROUP BY movie_lang, age_certificate;
+
+SELECT movie_lang, MIN(movie_length), MAX(movie_length) FROM movies
+WHERE age_certificate = '15'
+GROUP BY movie_lang;
+
 
