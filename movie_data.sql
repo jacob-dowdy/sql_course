876b7193c24141fd4c9ea1a06f5b3938a190f09b
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-07-30 07:59:55 -0400

    added join challenge 1

--- movie_data.sql
+++ movie_data.sql
@@ -1028,3 +1028,19 @@ SELECT mo.movie_name, mr.domestic_takings FROM movies mo
 JOIN movie_revenues mr USING (movie_id)
 WHERE mo.age_certificate IN ('12','15','18')
 ORDER BY mr.domestic_takings DESC;
+
+-- CHALLENGE 1 : JOINS
+
+SELECT d.first_name, d.last_name, mo.movie_name, mo.release_date FROM directors d 
+JOIN movies mo ON d.director_id = mo.director_id
+WHERE mo.movie_lang IN ('Chinese', 'Korean', 'Japanese');
+
+SELECT mo.movie_name, mo.release_date, mr.international_takings FROM movies mo
+JOIN movie_revenues mr ON mo.movie_id = mr.movie_id
+WHERE mo.movie_lang = 'English';
+
+SELECT mo.movie_name, mr.domestic_takings, mr.international_takings FROM movies mo
+JOIN movie_revenues mr ON mo.movie_id = mr.movie_id
+WHERE mr.domestic_takings IS NULL
+OR mr.international_takings IS NULL
+ORDER BY mo.movie_name;
