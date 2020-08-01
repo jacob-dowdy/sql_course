28b8f28901c5a38b2f0376bf3cac09afc858248d
jdowdy2123 <jacobdowdy@rocketmail.com>
2020-08-01 08:22:52 -0400

    added second joins challenge

--- movie_data.sql
+++ movie_data.sql
@@ -1091,6 +1091,15 @@ FULL JOIN movies mo ON d.director_id = mo.director_id
 WHERE mo.movie_lang IN ('German','Korean')
 ORDER BY d.last_name;
 
+-- CHALLENGE 2: JOINS
+
+SELECT d.first_name, d.last_name, mo.movie_name, mo.age_certificate FROM directors d
+LEFT JOIN movies mo ON d.director_id = mo.director_id
+WHERE d.nationality = 'British';
+
+SELECT d.first_name, d.last_name, COUNT (mo.movie_id) FROM directors d
+LEFT JOIN movies mo ON d.director_id = mo.director_id
+GROUP BY d.first_name, d.last_name;
 
 
 
