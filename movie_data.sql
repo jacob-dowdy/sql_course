4a1a1cd35cb39f2ad780d2b37ed0763773fdb421
Jacob Dowdy <jacobdowdy@rocketmail.com>
2021-05-29 08:26:32 -0400

    Update movie_data.sql

--- movie_data.sql
+++ movie_data.sql
@@ -1101,6 +1101,30 @@ SELECT d.first_name, d.last_name, COUNT (mo.movie_id) FROM directors d
 LEFT JOIN movies mo ON d.director_id = mo.director_id
 GROUP BY d.first_name, d.last_name;
 
+--- Joining More Than Two Tables
 
+/*
+SELECT t1.column, t2.column, t3.column FROM table t1
+JOIN table2 t2 ON t1.column = t2.column
+JOIN table3 t3 ON t3.column = t2.column;
+*/
+
+SELECT d.first_name, d.last_name, mo.movie_name, mr.international_takings, mr.domestic_takings
+FROM directors d
+JOIN movies mo ON d.director_id = mo.director_id
+JOIN movie_revenues mr ON mr.movie_id = mo.movie_id;
+
+SELECT ac.first_name, ac.last_name, mo.movie_name FROM actors ac
+JOIN movies_actors ma ON ac.actor_id = ma.actor_id
+JOIN movies mo ON mo.movie_id = ma.movie_id
+WHERE mo.movie_lang = 'English'
+ORDER BY mo.movie_name;
+
+SELECT d.first_name, d.last_name, mo.movie_name, ac.first_name, ac.last_name,
+mr.domestic_takings, mr.international_takings FROM directors d
+JOIN movies mo ON d.director_id = mo.director_id
+JOIN movies_actors ma ON ma.movie_id = mo.movie_id
+JOIN actors ac ON ac.actor_id = ma.actor_id
+JOIN movie_revenues mr ON mr.movie_id = mo.movie_id;
 
 
